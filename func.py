22d3505f8979eceff9238080658f94df31a1e272
SnowFox-95 <321mts95@gmail.com>
2022-07-14 23:13:44 +0300

    Функция сравнения числа с загаданным

--- func.py
+++ func.py
@@ -1,2 +1,19 @@
 def is_valid(n):
     return (n.isdigit() and int(n) in range(1, 101))
+
+
+def comparison(m, n):
+    while m != n:
+        if is_valid(n):
+            n = int(n)
+            if n < m:
+                print('Ваше число меньше загаданного, попробуйте еще раз')
+                n = input()
+            elif n > m:
+                print('Ваше число больше чем загадано. Лучше попробовать еще раз')
+                n = input()
+            else:
+                print('Может быть все-таки число от 1 до 100?')
+                n = input()
+
+    return 'Вы угадали, поздравляем!'
--- main.py
+++ main.py
@@ -4,10 +4,13 @@ import func
 mystery = randint(1, 100)
 print('Добро пожаловать в числовую угадайку')
 while True:
-    n = input('Введите число от 1 до 100:')
-    b = func.is_valid(n)
-    if func.is_valid(n):
-        number = int(n)
-        break
+    numb = input('Введите число от 1 до 100:')
+    if func.is_valid(numb):
+        print(func.comparison(mystery, numb))
+        print('Хотите сыграем еще раз?')
+        print('д - Да, н - Нет')
+        if input().lower() not in ['д', 'да', 'da', 'd', 'y', 'yes']:
+            print('Спасибо за игру в угадайку чисел. Увидимся...')
+            break
     else:
         print('А может все-таки впишем число от 1 до 100?')
