b84f7c90d37a94ea0c67f28a7e0a85bb1acd982c
SnowFox-95 <321mts95@gmail.com>
2022-07-14 23:02:32 +0300

    Основной цикл программы

--- main.py
+++ main.py
@@ -3,3 +3,11 @@ import func
 
 mystery = randint(1, 100)
 print('Добро пожаловать в числовую угадайку')
+while True:
+    n = input('Введите число от 1 до 100:')
+    b = func.is_valid(n)
+    if func.is_valid(n):
+        number = int(n)
+        break
+    else:
+        print('А может все-таки впишем число от 1 до 100?')
