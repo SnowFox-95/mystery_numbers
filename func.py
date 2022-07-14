4d3380e254e367140b723b9f23f3fde251bc0f7c
SnowFox-95 <321mts95@gmail.com>
2022-07-14 23:40:23 +0300

    Релиз 1.1 Счетчик попыток

--- func.py
+++ func.py
@@ -3,17 +3,23 @@ def is_valid(n):
 
 
 def comparison(m, n):
+    tryes = 0
     while m != n:
         if is_valid(n):
             n = int(n)
             if n < m:
                 print('Ваше число меньше загаданного, попробуйте еще раз')
+                tryes += 1
                 n = input()
             elif n > m:
                 print('Ваше число больше чем загадано. Лучше попробовать еще раз')
+                tryes += 1
                 n = input()
+            elif m == n:
+                break
             else:
                 print('Может быть все-таки число от 1 до 100?')
+                tryes += 1
                 n = input()
 
-    return 'Вы угадали, поздравляем!'
+    return 'Вы угадали, поздравляем! Потребовалось всего ' + str(tryes) + ' попыток'
--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 from random import *
 import func
 
-mystery = randint(1, 100)
+myst = randint(1, 100)
+
 print('Добро пожаловать в числовую угадайку')
+
 while True:
     numb = input('Введите число от 1 до 100:')
     if func.is_valid(numb):
-        print(func.comparison(mystery, numb))
+        print(func.comparison(myst, numb))
         print('Хотите сыграем еще раз?')
         print('д - Да, н - Нет')
         if input().lower() not in ['д', 'да', 'da', 'd', 'y', 'yes']:
             print('Спасибо за игру в угадайку чисел. Увидимся...')
             break
+        else:
+            pass
+
     else:
         print('А может все-таки впишем число от 1 до 100?')
